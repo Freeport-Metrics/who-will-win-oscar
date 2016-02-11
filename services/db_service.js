f7b36f7c18e7d0d2b71055274f87401cdeaf23b2
Kacper Trojanowski <kacper@freeportmetrics.com>
2016-02-11 11:47:12 +0100

    #19255 reorganized db_service code, changed structure of the cache, decoupled Socket.IO and DB

--- services/db_service.js
+++ services/db_service.js
@@ -13,33 +13,36 @@ module.exports = function (db, io) {
     time: '00:00'
   };
 
-  var toTime = function(h,m){
-    return ('0'+h).slice(-2) + ':' + ('0'+m).slice(-2)
+  var toTime = function (h, m) {
+    return ('0' + h).slice(-2) + ':' + ('0' + m).slice(-2)
   };
 
-  var currentTime = function() {
+  var currentTime = function () {
     var now = new Date();
     return {
       h: now.getUTCHours(),
       m: now.getUTCMinutes(),
-      time: toTime(now.getUTCHours(),now.getUTCMinutes())
+      time: toTime(now.getUTCHours(), now.getUTCMinutes())
     };
   };
 
-  var initialResult = function (minutesAgo,initialValues) {
-    var minutes = {};
-    if(!minutesAgo){
+  var initialResult = function (minutesAgo, initialValues) {
+    var minutes = [];
+    if (!minutesAgo) {
       minutesAgo = 60;
     }
     var now = new Date();
     var h = now.getUTCHours();
     var m = now.getUTCMinutes();
 
-    for (var i = minutesAgo; i > 0; i--) {
-      minutes[toTime(h,m--)] = initialValues ? initialValues : movies.reduce(function (obj, val) {
+    for (var i = 0; i < minutesAgo; i++) {
+      var counter = {};
+      counter[toTime(h, m--)] = initialValues ? initialValues : movies.reduce(function (obj, val) {
         obj[val] = 0;
         return obj;
       }, {});
+
+      minutes.push(counter);
       if (m < 0) {
         m = 59;
         h--;
@@ -48,25 +51,34 @@ module.exports = function (db, io) {
     return minutes;
   };
 
-  var cached_aggregated = null;
-  var cached_temp = null;
+  var aggregatedCache = null;
+  var tempCache = null;
+  var cacheCapacity = 60;
 
+  function updateCache(row, cache) {
+    var time = toTime(row.hour, row.minute);
 
-  function updateCache(row, cache){
-    var time = toTime(row.hour,row.minute);
-    if(!cache[time]){
+    if (!cache[0][time]) {
       var initial = cache[lastUpdate.time];
       var now = currentTime();
-      var minutesAgo = 60*(now.h - lastUpdate.h) + (now.min - lastUpdate.m);
-      var new_vals = initialResult(minutesAgo, initial);
-      extend(cache,new_vals);
-    }
-    row.movies.forEach(function(title){
-      cache[time][title] += 1;
-    });
-    lastUpdate = currentTime();
+      var minutesAgo = 60 * (now.h - lastUpdate.h) + (now.m - lastUpdate.m);
+      var newCounters = initialResult(minutesAgo, initial);
 
-    return cache[time];
+      newCounters.reverse().forEach(function (val) {
+        cache.unshift(val);
+      });
+
+      cache.splice(-minutesAgo, minutesAgo);
+    }
+    row.movies.forEach(function (title) {
+      cache[0][time][title] += 1;
+    });
+    lastUpdate = {
+      h: time.split(':')[0],
+      m: time.split(':')[1],
+      time: time
+    };
+    return cache[0];
   }
 
 
@@ -101,75 +113,118 @@ module.exports = function (db, io) {
             var title = row['group'].splice(0, 1)[0];
             var h = row['group'][0];
             var m = row['group'][1];
-            var time = toTime(h,m);
+            var time = toTime(h, m);
 
-            if (!result[time]) {
-              return;
-            }
-            result[time][title] = row.reduction;
-            Object.keys(aggregated_result).forEach(function (key) {
-              if (key > time) {
-                aggregated_result[key][title] += row.reduction;
 
+            result.forEach(function (counter) {
+              if (counter[time]) {
+                counter[time][title] = row.reduction;
+                return false;
               }
             });
-
+            aggregated_result.forEach(function (counter) {
+              var key = Object.keys(counter)[0];
+              if (key == time || key > time) {
+                counter[key][title] += row.reduction;
+              }
+            });
           }, function () {
-            cached_aggregated = aggregated_result;
-            cached_temp = result;
-            lastUpdate = currentTime();
-            if(callback) {
+            aggregatedCache = aggregated_result;
+            tempCache = result;
+            var last_key = Object.keys(aggregatedCache[0])[0];
+            lastUpdate = {
+              h: last_key.split(':')[0],
+              m: last_key.split(':')[1],
+              time: last_key
+            };
+            if (callback) {
               callback();
             }
           });
     });
   }
 
-  function sendCache(socket){
-    socket.emit('initialize_tweet_aggregated', cached_aggregated);
-    socket.emit('initialize_tweet_not_aggregated', cached_temp);
+  function sendCache(socket) {
+    socket.emit('initialize_tweet_aggregated', aggregatedCache);
+    socket.emit('initialize_tweet_not_aggregated', tempCache);
   }
+
+  function listenForChanges(conn, callback) {
+    db.r.table('Tweet').changes(
+        {
+          squash: true
+        }
+    ).map(function (row) {
+      return {
+        hour: row('new_val')('created_at').hours(),
+        minute: row('new_val')('created_at').minutes(),
+        date: row('new_val')('created_at'),
+        text: row('new_val')('text'),
+        is_new: row('old_val').eq(null),
+        movies: row('new_val')('movies')
+      };
+    }).run(conn, function (err, cursor) {
+      cursor.each(function (err, row) {
+        if (err) {
+          throw err;
+        }
+
+
+        if (!row.is_new) {
+          return;
+        }
+        var lastCounterAggregated = updateCache(row, aggregatedCache);
+        var lastCounter = updateCache(row, tempCache);
+        if (callback) {
+          callback(row, lastCounter, lastCounterAggregated);
+        }
+
+      }, function () {
+        // finished processing
+      });
+    });
+  }
+
+  function findClientsSocket(roomId, namespace) {
+    var res = [];
+    var ns = io.of(namespace || "/");    // the default namespace is "/"
+
+    if (ns) {
+      for (var id in ns.connected) {
+        if (roomId) {
+          var index = ns.connected[id].rooms.indexOf(roomId);
+          if (index !== -1) {
+            res.push(ns.connected[id]);
+          }
+        } else {
+          res.push(ns.connected[id]);
+        }
+      }
+    }
+    return res;
+  }
+
+  function sendCountersToActiveSockets(tweet, lastCounter, lastCounterAggregated) {
+
+    findClientsSocket().forEach(function (socket) {
+      socket.emit('new_tweets_aggregates', lastCounterAggregated);
+      socket.emit('new_tweets', lastCounter);
+      socket.emit('tweet', tweet);
+    });
+
+  }
+
 
   db.conn.then(function (conn) {
 
-    initialize(conn, function(){
+    initialize(conn, function () {
+
+      var sockets = [];
+      listenForChanges(conn, sendCountersToActiveSockets);
+
       io.on('connection', function (socket) {
         sendCache(socket);
-        db.r.table('Tweet').changes(
-            {
-              squash: true
-            }
-        ).map(function(row){
-          return {
-            hour: row('new_val')('created_at').hours(),
-            minute: row('new_val')('created_at').minutes(),
-            date: row('new_val')('created_at'),
-            text: row('new_val')('text'),
-            is_new: row('old_val').eq(null),
-            movies: row('new_val')('movies')
-          };
-        }).run(conn, function (err, cursor) {
-          cursor.each(function (err, row)  {
-            if (err) {
-              throw err;
-            }
-
-
-            if(!row.is_new){
-              return;
-            }
-            var lastValueAgg = updateCache(row,cached_aggregated);
-            var lastValue = updateCache(row,cached_temp);
-
-            console.log(lastValueAgg);
-            console.log('<<<<<<<>>>>>>>');
-            socket.emit('new_tweets_aggregates',lastValueAgg);
-            socket.emit('new_tweets',lastValue);
-            socket.emit('tweet', row);
-          }, function () {
-            // finished processing
-          });
-        });
+        sockets.push(socket);
         socket.on('disconnect', function (socket) {
 
         })
@@ -178,7 +233,7 @@ module.exports = function (db, io) {
 
 
   })
-}
+};
 
 
 
