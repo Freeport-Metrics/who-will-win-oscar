86b9e87a87c6551b0825290f2382d5398b107587
Jan Terlecki <jan@freeportmetrics.com>
2016-03-08 11:56:00 +0100

    removing double quotes

--- deploy/before_deploy.rb
+++ deploy/before_deploy.rb
@@ -1,3 +1,2 @@
-#!/bin/sh
-run "rm /usr/local/bin/ey_node_app_info"
-run "ln -s /opt/nodejs/current/bin/ey_node_app_info /usr/local/bin/ey_node_app_info"
+run 'rm /usr/local/bin/ey_node_app_info'
+run 'ln -s /opt/nodejs/current/bin/ey_node_app_info /usr/local/bin/ey_node_app_info'
