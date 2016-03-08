0835502223e5b8048f4dbf41081554d98278f23c
Jan Terlecki <jan@freeportmetrics.com>
2016-03-08 11:59:12 +0100

    Changing run to sudo

--- deploy/before_deploy.rb
+++ deploy/before_deploy.rb
@@ -1,2 +1,2 @@
-run 'rm /usr/local/bin/ey_node_app_info'
-run 'ln -s /opt/nodejs/current/bin/ey_node_app_info /usr/local/bin/ey_node_app_info'
+sudo 'rm /usr/local/bin/ey_node_app_info'
+sudo 'ln -s /opt/nodejs/current/bin/ey_node_app_info /usr/local/bin/ey_node_app_info'
